136253e99cb2ffbdaec3ca2bd34df4ad65135c8f
Andrea Frigo <andrea.boy.frigo@gmail.com>
2019-05-01 18:34:01 +0200

    Inserite relazioni

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -10,6 +10,23 @@ CREATE TABLE paziente
     codice_fiscale VARCHAR(16) NOT NULL,
     sesso CHAR(1) NOT NULL,
     foto TEXT, 
+    id_medico INT REFERENCES medico_base(id) NOT NULL
+);
+
+CREATE TABLE visita_base(
+    id INT SERIAL NOT NULL,
+    id_medico INT REFERENCES medico_base(id) NOT NULL,
+    id_paziente INT REFERENCES paziente(id) NOT NULL,
+);
+
+CREATE TABLE prescrive_ricetta(
+    id_visita INT REFERENCES visita_base(id) NOT NULL,
+    id_ricetta INT REFERENCES ricetta(id) NOT NULL,
+);
+
+CREATE TABLE prescrive_visita(
+    id_visita INT REFERENCES visita_base(id) NOT NULL,
+    id_visita_medica INT REFERENCES visita_medica(id) NOT NULL,
 );
 
 CREATE TABLE medico_base
@@ -32,13 +49,21 @@ CREATE TABLE report
     esito TEXT NOT NULL
 );
 
+CREATE TABLE visita_specialistica(
+    id_medico INT REFERENCES medico_specialista(id) NOT NULL,
+    id_paziente INT REFERENCES paziente(id) NOT NULL,
+    id_report INT REFERENCES report(id) NOT NULL,
+    ticket INT NOT NULL
+);
+
+
 CREATE TABLE visita_medica
 (
     id INT SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE farmaco
+CREATE TABLE ricetta
 (
     id INT SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
