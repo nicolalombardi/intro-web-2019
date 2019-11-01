01bf8a16f9aa8fac1343f71979f0ee8181143a27
Nicola Lombardi <lombardi.nicola23@gmail.com>
2019-11-01 20:45:23 +0100

    Rimossa visita medica dal progetto

--- app/src/main/java/com/icecoldbier/persistence/dao/interfaces/VisitaMedicaDAOInterface.java
+++
@@ -1,7 +0,0 @@
-package com.icecoldbier.persistence.dao.interfaces;
-
-import com.icecoldbier.persistence.entities.VisitaMedica;
-
-public interface VisitaMedicaDAOInterface {
-    VisitaMedica getVisitaMedica(Integer id);
-}
