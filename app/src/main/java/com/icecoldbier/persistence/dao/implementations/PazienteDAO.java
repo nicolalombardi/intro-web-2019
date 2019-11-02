fd368eb19c8669da87d79e6f74909be5d423e4db
Nicola Lombardi <lombardi.nicola23@gmail.com>
2019-11-02 12:04:10 +0100

    Aggiunte due implementazione della query per la lista delle visite di un paziente

--- app/src/main/java/com/icecoldbier/persistence/dao/implementations/PazienteDAO.java
+++ app/src/main/java/com/icecoldbier/persistence/dao/implementations/PazienteDAO.java
@@ -1,18 +1,24 @@
 package com.icecoldbier.persistence.dao.implementations;
 
 import com.icecoldbier.persistence.dao.interfaces.PazienteDAOInterface;
-import com.icecoldbier.persistence.entities.Paziente;
+import com.icecoldbier.persistence.entities.*;
 import it.unitn.disi.wp.commons.persistence.dao.exceptions.DAOException;
 import it.unitn.disi.wp.commons.persistence.dao.jdbc.JDBCDAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PazienteDAO extends JDBCDAO<Paziente, Integer> implements PazienteDAOInterface {
     private static final String GET_PAZIENTE_BY_ID = "SELECT * FROM paziente WHERE id_user = ?";
+
+    //These are for the separate queries approach
+    private static final String GET_VISITE_BASE = "SELECT * FROM visita_base WHERE id_paziente = ?";
+    private static final String GET_VISITE_SPECIALISTICHE = "SELECT * FROM visita_specialistica WHERE id_paziente = ?";
+    private static final String GET_VISITE_SSP = "SELECT * FROM visita_ssp WHERE id_paziente = ?";
+
+    //This is for the single query approach
+    private static final String GET_ALL_VISITE = "SELECT 'specialistica' as type, id, id_visita, erogata, data_prescrizione, data_erogazione, id_medico, id_paziente, id_report, NULL AS id_ssp from visita_specialistica WHERE id_paziente = ? UNION SELECT 'ssp' AS type, id, id_visita, erogata, data_prescrizione, data_erogazione, NULL AS id_medico, id_paziente, NULL AS id_report, id_ssp from visita_ssp WHERE id_paziente = ? UNION SELECT 'base' AS type, id, NULL AS id_visita, NULL AS erogata, NULL AS data_prescrizione, data_erogazione, id_medico, id_paziente, NULL AS id_report, NULL AS id_ssp FROM visita_base WHERE id_paziente = ?;";
 
     /**
      * The base constructor for all the JDBC DAOs.
@@ -61,5 +67,124 @@ public class PazienteDAO extends JDBCDAO<Paziente, Integer> implements PazienteD
         } catch (SQLException e) {
             throw new DAOException("Error while getting paziente", e);
         }
+    }
+
+    @Override
+    public ArrayList<Visita> getVisiteSingleQuery(Integer id) throws DAOException {
+        ArrayList<Visita> visite = new ArrayList<>();
+
+        try(PreparedStatement preparedStatement = CON.prepareStatement(GET_ALL_VISITE)){
+            preparedStatement.setInt(1, id);
+            preparedStatement.setInt(2, id);
+            preparedStatement.setInt(3, id);
+
+            try (ResultSet rs = preparedStatement.executeQuery()){
+                while (rs.next()){
+                    Visita.VisitaType type = Visita.VisitaType.valueOf(rs.getString("type").toUpperCase());
+                    if(type == Visita.VisitaType.BASE){
+                        VisitaBase visitaBase = new VisitaBase(
+                            rs.getInt("id"),
+                            rs.getInt("id_medico"),
+                            rs.getInt("id_paziente"),
+                            rs.getDate("data_erogazione")
+                    );
+                    visite.add(visitaBase);
+                    }
+                    else if(type == Visita.VisitaType.SPECIALISTICA){
+                        VisitaSpecialistica visitaSpecialistica = new VisitaSpecialistica(
+                            rs.getInt("id"),
+                            rs.getInt("id_visita"),
+                            rs.getBoolean("erogata"),
+                            rs.getDate("data_prescrizione"),
+                            rs.getDate("data_erogazione"),
+                            rs.getInt("id_medico"),
+                            rs.getInt("id_paziente"),
+                            rs.getInt("id_report")
+                    );
+                    visite.add(visitaSpecialistica);
+                    }
+                    if(type == Visita.VisitaType.SSP){
+                        VisitaSSP visitaSSP = new VisitaSSP(
+                            rs.getInt("id"),
+                            rs.getInt("id_visita"),
+                            rs.getBoolean("erogata"),
+                            rs.getDate("data_prescrizione"),
+                            rs.getDate("data_erogazione"),
+                            rs.getInt("id_ssp"),
+                            rs.getInt("id_paziente")
+                    );
+                    visite.add(visitaSSP);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DAOException("Error while getting list of visite", e);
+        }
+        return visite;
+    }
+
+    @Override
+    public ArrayList<Visita> getVisiteMultipleQueries(Integer id) throws DAOException {
+        ArrayList<Visita> visite = new ArrayList<>();
+
+        try (PreparedStatement preparedStatement = CON.prepareStatement(GET_VISITE_BASE)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                while (rs.next()) {
+                    VisitaBase visitaBase = new VisitaBase(
+                            rs.getInt("id"),
+                            rs.getInt("id_medico"),
+                            rs.getInt("id_paziente"),
+                            rs.getDate("data_erogazione")
+                    );
+                    visite.add(visitaBase);
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DAOException("Impossible to get the list of visite base", ex);
+        }
+
+        try (PreparedStatement preparedStatement = CON.prepareStatement(GET_VISITE_SPECIALISTICHE)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                while (rs.next()) {
+                    VisitaSpecialistica visitaSpecialistica = new VisitaSpecialistica(
+                            rs.getInt("id"),
+                            rs.getInt("id_visita"),
+                            rs.getBoolean("erogata"),
+                            rs.getDate("data_prescrizione"),
+                            rs.getDate("data_erogazione"),
+                            rs.getInt("id_medico"),
+                            rs.getInt("id_paziente"),
+                            rs.getInt("id_report")
+                    );
+                    visite.add(visitaSpecialistica);
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DAOException("Impossible to get the list of visite specialistiche", ex);
+        }
+
+        try (PreparedStatement preparedStatement = CON.prepareStatement(GET_VISITE_SSP)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                while (rs.next()) {
+                    VisitaSSP visitaSSP = new VisitaSSP(
+                            rs.getInt("id"),
+                            rs.getInt("id_visita"),
+                            rs.getBoolean("erogata"),
+                            rs.getDate("data_prescrizione"),
+                            rs.getDate("data_erogazione"),
+                            rs.getInt("id_ssp"),
+                            rs.getInt("id_paziente")
+                    );
+                    visite.add(visitaSSP);
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DAOException("Impossible to get the list of visite ssp", ex);
+        }
+
+        return visite;
     }
 }
--- app/src/main/java/com/icecoldbier/persistence/dao/interfaces/PazienteDAOInterface.java
+++ app/src/main/java/com/icecoldbier/persistence/dao/interfaces/PazienteDAOInterface.java
@@ -1,9 +1,14 @@
 package com.icecoldbier.persistence.dao.interfaces;
 
 import com.icecoldbier.persistence.entities.Paziente;
+import com.icecoldbier.persistence.entities.Visita;
 import it.unitn.disi.wp.commons.persistence.dao.DAO;
 import it.unitn.disi.wp.commons.persistence.dao.exceptions.DAOException;
 
+import java.util.ArrayList;
+
 public interface PazienteDAOInterface extends DAO<Paziente, Integer> {
     Paziente getPaziente(Integer id) throws DAOException;
+    ArrayList<Visita> getVisiteSingleQuery(Integer id) throws DAOException;
+    ArrayList<Visita> getVisiteMultipleQueries(Integer id) throws DAOException;
 }
