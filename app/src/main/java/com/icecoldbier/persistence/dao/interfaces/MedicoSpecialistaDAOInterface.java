1771ffe53dd62699686d91b6aaea9fa7a6f431bc
Aurelio Pennacchio <aurelio99819@gmail.com>
2019-11-04 22:34:29 +0100

    Aggiunti metodi delle funzionalità medicoSpecialista

--- app/src/main/java/com/icecoldbier/persistence/dao/interfaces/MedicoSpecialistaDAOInterface.java
+++ app/src/main/java/com/icecoldbier/persistence/dao/interfaces/MedicoSpecialistaDAOInterface.java
@@ -1,8 +1,17 @@
 package com.icecoldbier.persistence.dao.interfaces;
 
+import com.icecoldbier.persistence.entities.Paziente;
+import com.icecoldbier.persistence.entities.Report;
 import com.icecoldbier.persistence.entities.User;
+import com.icecoldbier.persistence.entities.VisitaSpecialistica;
 import it.unitn.disi.wp.commons.persistence.dao.DAO;
+import it.unitn.disi.wp.commons.persistence.dao.exceptions.DAOException;
+import java.util.ArrayList;
 
 public interface MedicoSpecialistaDAOInterface extends DAO<User, Integer> {
-
+    public ArrayList <Paziente> getListaPazientiAssociati(int id) throws DAOException;
+    public ArrayList <VisitaSpecialistica> getListaVisitePazienti() throws DAOException;
+    public Paziente getInfoPaziente(int id) throws DAOException;
+    public void erogaVisita(int idv, Report report) throws DAOException;
+    public void erogaVisitaConRicetta(int idv, Report report) throws DAOException;
 }
