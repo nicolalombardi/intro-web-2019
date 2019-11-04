7c14c818a20741467fff53afc03d829b41a0a829
Andrea Frigo <andrea.boy.frigo@gmail.com>
2019-11-04 11:42:15 +0100

    Aggiunta funzione in utils per l'invio delle mail

--- app/src/main/java/com/icecoldbier/utils/Utils.java
+++ app/src/main/java/com/icecoldbier/utils/Utils.java
@@ -1,6 +1,10 @@
 package com.icecoldbier.utils;
 
 import javax.servlet.ServletContext;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Date;
+import java.util.Properties;
 
 public class Utils {
     public static String getServletContextPath(ServletContext context){
@@ -9,5 +13,32 @@ public class Utils {
             contextPath += "/";
         }
         return contextPath;
+    }
+
+    // "uname" e "to" sono indirizzi email
+    public void sendMail(final String uname, final String psw, String to, String subject, String text) throws MessagingException {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.port", 587);
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.debug", "true");
+        prop.setProperty("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(uname, psw);
+            }
+        };
+
+        Session session = Session.getInstance(prop, auth);
+        InternetAddress[] mailTo = InternetAddress.parse(to);
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(uname));
+        message.setRecipients(Message.RecipientType.TO, mailTo);
+        message.setSubject(subject);
+        message.setSentDate(new Date());
+        message.setText(text);
+
+        Transport.send(message);
     }
 }
