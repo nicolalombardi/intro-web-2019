d13420524ca4ce6bcfcbabfaafa8514443e5aa71
Andrea Frigo <andrea.boy.frigo@gmail.com>
2019-11-05 12:51:48 +0100

    Sistemati metodi dopo modifica db

--- app/src/main/java/com/icecoldbier/persistence/dao/implementations/SSPDAO.java
+++ app/src/main/java/com/icecoldbier/persistence/dao/implementations/SSPDAO.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class SSPDAO extends JDBCDAO<SSP, Integer> implements SSPDAOInterface {
     private static final String EROGA_VISITA_BY_ID = "UPDATE visita_ssp SET erogata = TRUE, data_erogazione = NOW() WHERE id = ?";
     private static final String GET_VISITE = "SELECT v.data_erogazione AS data, r.farmaco AS farmaco, v.id_medico AS medico, v.id_paziente AS paziente FROM visita_base v, ricetta r, users m WHERE r.id_visita_base = v.id AND v.id_medico = m.id AND v.data_erogazione = CAST ( ? AS date) AND m.provincia_appartenenza = ?";
+    private static final String GET_SSP_BY_ID = "SELECT * FROM users WHERE id = ?";
+
 
 
     /**
@@ -42,11 +44,13 @@ public class SSPDAO extends JDBCDAO<SSP, Integer> implements SSPDAOInterface {
         ArrayList<InfoRicetta> list = new ArrayList<InfoRicetta>();
         try (PreparedStatement preparedStatement = CON.prepareStatement(GET_VISITE)) {
             preparedStatement.setDate(1, data);
-            preparedStatement.setString(2, ssp.getProvinciaAppartenenza());
+            String prov = ssp.getProvinciaAppartenenza();
+            preparedStatement.setString(2, prov);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while(resultSet.next()){
                 list.add(new InfoRicetta(resultSet.getDate("data"), resultSet.getString("farmaco"), resultSet.getInt("medico"), resultSet.getInt("paziente")));
+                System.out.println("Aggiunta una visita alla lista");
             }
         } catch (SQLException e) {
             throw new DAOException("Error while getting lista ricette erogate in un giorno ", e);
@@ -64,7 +68,21 @@ public class SSPDAO extends JDBCDAO<SSP, Integer> implements SSPDAOInterface {
 
     @Override
     public SSP getByPrimaryKey(Integer primaryKey) throws DAOException {
-        return null;
+        SSP ssp = null;
+        try(PreparedStatement preparedStatement = CON.prepareStatement(GET_SSP_BY_ID)){
+            preparedStatement.setInt(1, primaryKey);
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                resultSet.next();
+                ssp = new SSP(
+                        resultSet.getInt("id"),
+                        resultSet.getString("provincia_appartenenza")
+                );
+            }
+
+        } catch (SQLException e) {
+            throw new DAOException("Error while getting report", e);
+        }
+        return ssp;
     }
 
     @Override
