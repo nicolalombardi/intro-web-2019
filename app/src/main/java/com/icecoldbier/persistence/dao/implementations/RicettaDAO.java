1be8457799884dda312bd42c9ff3945a87771921
Andrea Frigo <andrea.boy.frigo@gmail.com>
2020-01-02 11:26:55 +0100

    Implementata getCount per ricette

--- app/src/main/java/com/icecoldbier/persistence/dao/implementations/RicettaDAO.java
+++ app/src/main/java/com/icecoldbier/persistence/dao/implementations/RicettaDAO.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 public class RicettaDAO extends JDBCDAO<Ricetta, Integer> implements RicettaDAOInterface {
     private static final String GET_RICETTA_BY_ID = "SELECT * FROM ricetta WHERE id = ?";
+    private static final String GET_RICETTE_COUNT = "SELECT COUNT(id) as count FROM ricetta r, visita_base vb, report rep, visita_specialistica vs WHERE (r.id = vb.id_ricetta AND vb.id_paziente = ?) OR (r.id = rep.id_ricetta AND rep.id = vs.id_report AND vs.id_paziente = ?) ";
 
     /**
      * The base constructor for all the JDBC DAOs.
@@ -25,6 +26,20 @@ public class RicettaDAO extends JDBCDAO<Ricetta, Integer> implements RicettaDAOI
         super(con);
     }
 
+    @Override
+    public Long getCount(int idp) throws DAOException {
+        try (PreparedStatement preparedStatement = CON.prepareStatement((GET_RICETTE_COUNT))){
+            preparedStatement.setInt(1,idp);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                resultSet.next();
+                return resultSet.getLong("count");
+            }
+        } catch (SQLException ex) {
+            throw new DAOException("Impossible to get the count of Ricetta", ex);
+        }
+    }
+
+    @Override
     public Long getCount() throws DAOException {
         return null;
     }
--- app/src/main/java/com/icecoldbier/persistence/dao/interfaces/RicettaDAOInterface.java
+++ app/src/main/java/com/icecoldbier/persistence/dao/interfaces/RicettaDAOInterface.java
@@ -2,6 +2,8 @@ package com.icecoldbier.persistence.dao.interfaces;
 
 import com.icecoldbier.persistence.entities.Ricetta;
 import it.unitn.disi.wp.commons.persistence.dao.DAO;
+import it.unitn.disi.wp.commons.persistence.dao.exceptions.DAOException;
 
 public interface RicettaDAOInterface extends DAO<Ricetta, Integer> {
+    public Long getCount(int idp) throws DAOException;
 }
