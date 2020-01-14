2b50c6334cde82e1cfaeac02e31b4b0459925196
Nicola Lombardi <lombardi.nicola23@gmail.com>
2020-01-15 00:18:13 +0100

    Implementato multi-threading per l'invio delle email

--- app/src/main/java/com/icecoldbier/utils/Utils.java
+++ app/src/main/java/com/icecoldbier/utils/Utils.java
@@ -5,6 +5,8 @@ import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.Date;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Utils {
     public static String getServletContextPath(ServletContext context){
@@ -22,34 +24,45 @@ public class Utils {
      * @param text Testo della mail
      * @throws MessagingException
      */
-    public static void sendMail(String to, String subject, String text) throws MessagingException {
+    public static void sendMail(final String to, final String subject, final String text) throws MessagingException {
         final String accountEmail = "progettoweb0@gmail.com";
         final String accountPassword = "icecoldbier";
         final String senderAddress = "progettoweb0+admin@gmail.com";
 
-        Properties prop = new Properties();
-        prop.put("mail.smtp.port", 587);
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.debug", "true");
-        prop.setProperty("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true");
+        final ExecutorService emailExecutor = Executors.newSingleThreadExecutor();
+        emailExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Properties prop = new Properties();
+                    prop.put("mail.smtp.port", 587);
+                    prop.put("mail.smtp.host", "smtp.gmail.com");
+                    prop.put("mail.debug", "true");
+                    prop.setProperty("mail.smtp.auth", "true");
+                    prop.put("mail.smtp.starttls.enable", "true");
 
-        Authenticator auth = new Authenticator() {
-            public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(accountEmail, accountPassword);
+                    Authenticator auth = new Authenticator() {
+                        public PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(accountEmail, accountPassword);
+                        }
+                    };
+
+                    Session session = Session.getInstance(prop, auth);
+                    InternetAddress[] mailTo = InternetAddress.parse(to);
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(senderAddress));
+                    message.setRecipients(Message.RecipientType.TO, mailTo);
+                    message.setSubject(subject);
+                    message.setSentDate(new Date());
+                    message.setText(text);
+
+                    Transport.send(message);
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
             }
-        };
-
-        Session session = Session.getInstance(prop, auth);
-        InternetAddress[] mailTo = InternetAddress.parse(to);
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(senderAddress));
-        message.setRecipients(Message.RecipientType.TO, mailTo);
-        message.setSubject(subject);
-        message.setSentDate(new Date());
-        message.setText(text);
-
-        Transport.send(message);
+        });
+        emailExecutor.shutdown();
     }
 
     public static int coerceInt(int min, int max, int value){
